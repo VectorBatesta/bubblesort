84966fb660211bd5b631dc136e42ef9379f2bc41
VectorBatesta <48493157+VectorBatesta@users.noreply.github.com>
2020-03-06 09:15:11 -0300

    literally random numbers and dynamic array

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 void bubbleSort(int* vetor, int tamvetor){
   int aux;
-  for(int j=0; j<50; j++){
+  for(int j=0; j<tamvetor; j++){
     for(int i=0; i<tamvetor-1; i++){
       if(vetor[i+1] < vetor[i]){
         aux = vetor[i];
@@ -14,12 +15,38 @@ void bubbleSort(int* vetor, int tamvetor){
   }
 }
 
+
+
+
 int main(){
-  int vetor[50] = {23,45,234,52,345,2345,2,345,234,5,234,6,435,67456,7,5678,567,8,67,345,6143,3567,257,245,62,34,525,73,457,37};
-  int tamvetor = 50;
+  printf("tamanho do vetor: ");
+  int tamvetor;
+  scanf("%i", &tamvetor);
+  getchar();
+
+  int *vetor = malloc(tamvetor * sizeof *vetor);
+
+  srand(time(NULL));
+  for(int i=0; i<tamvetor; i++){
+    if(i > tamvetor/2)
+      vetor[i] = rand()%10000;
+    else if(i > tamvetor/3)
+      vetor[i] = rand()%1000;
+    else
+      vetor[i] = rand()%100;
+  }
+
+  for(int i=0; i<tamvetor; i++){
+    printf("%i,\n", vetor[i]);
+  }
+
+  printf("\n[pressione ENTER para continuar]");
+  getchar();
+  system("clear");
 
   bubbleSort(vetor,tamvetor);
 
+  printf("vetor BUBBLESORTADO:\n");
   for (size_t i = 0; i < tamvetor; i++) {
     printf("%i,\n", vetor[i]);
   }
